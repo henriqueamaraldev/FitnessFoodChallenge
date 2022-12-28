f1bb65f1127abce8f9dbe545c79af4a1b76f73c4
Henrique Correa do Amaral <henrique.amaral@rhitmo.com.br>
2022-12-28 14:42:11 -0300

    feat(logger): trying to implement logger for cron

--- FitnessFoodChallenge/Cron.Service/services/OpenFoodFactsCron.cs
+++ FitnessFoodChallenge/Cron.Service/services/OpenFoodFactsCron.cs
@@ -1,12 +1,29 @@
-﻿using Quartz;
+﻿using Microsoft.Extensions.Logging;
+using Quartz;
 
 namespace Cron.Service.services
 {
     public class OpenFoodFactsCron : IJob
     {
+        private readonly ILogger<OpenFoodFactsCron> _logger;
+        public OpenFoodFactsCron(ILogger<OpenFoodFactsCron> logger)
+        {
+            _logger = logger;
+        }
         public Task Execute(IJobExecutionContext context)
         {
-            throw new NotImplementedException();
+            throw new Exception();
+
+            /*
+            {
+                return Task.CompletedTask;
+                throw new Exception();
+                
+            }
+            catch (Exception e)
+            {
+                _logger.LogError("FoodJob error", e);
+            }*/
         }
     }
 }
