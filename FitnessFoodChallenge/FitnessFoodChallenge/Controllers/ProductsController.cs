7b4e6d827628759327277f99e5131bad767c4edd
Henrique Correa do Amaral <henrique.amaral@rhitmo.com.br>
2022-12-28 14:59:58 -0300

    feat(controller): adding products controller

---
+++ FitnessFoodChallenge/FitnessFoodChallenge/Controllers/ProductsController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FitnessFoodChallenge.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        [HttpGet]
+        [Route("/")]
+        public string HelloWorld()
+        {
+            return "Fullstack Challenge 20201026";
+        }
+
+        [HttpGet]
+        [Route("products")]
+        public string GetProducts()
+        {
+            return "Not implemented yet";
+        }
+
+        [HttpGet]
+        [Route("products/{id}")]
+        public string GetProduct(long id)
+        {
+            return "Not implemented yet";
+        }
+    }
+}
