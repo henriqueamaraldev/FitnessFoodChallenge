e56ba3aab318edc364a6ddd86e85408805cebaf7
Henrique Correa do Amaral <henrique.amaral@rhitmo.com.br>
2022-12-29 13:47:18 -0300

    feat(mongo): base model created

---
+++ FitnessFoodChallenge/Repository/models/BaseModel.cs
@@ -0,0 +1,12 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+
+namespace Repository.models
+{
+    public abstract class BaseEntity
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public long Id { get; private set; }
+    }
+}
